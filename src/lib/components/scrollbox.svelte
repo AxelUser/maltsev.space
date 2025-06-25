041a436d7651ef5bd1e697f87b01594b57711677
Aleksey Maltsev <axel.user.perm@gmail.com>
2025-06-25 13:29:35 +0200

    feat: add Scrollbox component (#13)

---
+++ src/lib/components/scrollbox.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	const { children } = $props();
+</script>
+
+<div class="scrollbox">
+	{@render children()}
+</div>
+
+<style>
+	.scrollbox {
+		overflow-x: auto;
+		max-inline-size: fit-content;
+	}
+</style>
